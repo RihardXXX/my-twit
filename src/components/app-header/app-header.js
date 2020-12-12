6a6257cebb7985a4623cb27f6f88f02392cb33ff
RihardXXX <yusupa.akaeva@yandex.ru>
2020-12-12 14:27:40 +0300

    create components

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AppHeader = () => {
+    return(
+        <div className="app-header d-flex">
+            <h1>Rihard Alikhanov</h1>
+            <h2>3 записи, из них понравилось 0</h2>
+        </div>
+    )
+}
+
+export default AppHeader
